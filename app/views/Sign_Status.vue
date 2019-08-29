e50c728ad475ab729534c1fc33c2224b1d59f358
김현진 <khjini3@hanmail.net>
2019-08-29 16:27:29 +0900

    tt

--- app/views/Sign_Status.vue
+++ app/views/Sign_Status.vue
@@ -50,21 +50,42 @@
 
     import * as utils from "~/service/utils/utils"
     import SelectedPageService from "~/service/utils/selected-page-service";
+    import ListComp from "../components/sign_status/Sign-List-Comp";
 
     export default {
         mounted(){
 
         },
 
+        components () {
+            ListComp    
+        },
+
         data (){
             return {
 
+                dataProvider : [
+                    {part:'연구1팀', name : '김현진', type : '외근', sign_date : '2019-08-27 09:20:12', period : '2019-08-27', time:'09:00 ~ 18:00', days:'5일'},
+                    {part:'연구1팀', name : '김현진', type : '장기외근', sign_date : '2019-08-27 09:20:12', period : '2019-08-27 ~ 2019-08-31', time:'', days:''},
+                    {part:'연구1팀', name : '김현진', type : '연차휴가', sign_date : '2019-08-27 09:20:12', period : '2019-08-27 ~ 2019-08-31 ', time:'', days:'5일'},
+                    {part:'연구1팀', name : '김현진', type : '오전반차', sign_date : '2019-08-27 09:20:12', period : '2019-08-27', time:'', days:'0.5일'},
+                    {part:'연구1팀', name : '김현진', type : '초과근무', sign_date : '2019-08-27 09:20:12', period : '2019-08-27', time:'', days:''},
+                    {part:'연구1팀', name : '김현진', type : '외근', sign_date : '2019-08-27 09:20:12', period : '2019-08-27', time:'09:00 ~ 18:00', days:'5일'},
+                    {part:'연구1팀', name : '김현진', type : '장기외근', sign_date : '2019-08-27 09:20:12', period : '2019-08-27 ~ 2019-08-31', time:'', days:''},
+                    {part:'연구1팀', name : '김현진', type : '연차휴가', sign_date : '2019-08-27 09:20:12', period : '2019-08-27 ~ 2019-08-31 ', time:'', days:'5일'},
+                    {part:'연구1팀', name : '김현진', type : '오전반차', sign_date : '2019-08-27 09:20:12', period : '2019-08-27', time:'', days:'0.5일'},
+                    {part:'연구1팀', name : '김현진', type : '초과근무', sign_date : '2019-08-27 09:20:12', period : '2019-08-27', time:'', days:''}
+                ]
             }
         },
 
         methods : {
              onDrawerButtonTap() {
                 utils.showDrawer();
+            },
+
+            onItemTap(e){
+                console.log(e.item);
             }
         }
     }
