6afc6d24ddf77974941e840106badf11007ae653
GihwanLee <lgh816@hanmail.net>
2019-09-02 17:49:21 +0900

    근태 현황 Component 추가
    근태 현황 Component 추가

--- app/views/Commute.vue
+++ app/views/Commute.vue
@@ -1,46 +1,19 @@
 <template>
-    <Page class="page">
-        <ActionBar class="action-bar">
-            <!-- 
-            Use the NavigationButton as a side-drawer button in Android
-            because ActionItems are shown on the right side of the ActionBar
-            -->
-            <NavigationButton ios:visibility="collapsed" icon="res://menu" @tap="onDrawerButtonTap"></NavigationButton>
-            <!-- 
-            Use the ActionItem for IOS with position set to left. Using the
-            NavigationButton as a side-drawer button in iOS is not possible,
-            because its function is to always navigate back in the application.
-            -->
-            <ActionItem icon="res://navigation/menu" 
-                android:visibility="collapsed" 
-                @tap="onDrawerButtonTap"
-                ios.position="left">
-            </ActionItem>
-            <Label class="action-bar-title" text="근태 현황"></Label>
-        </ActionBar>
-
-        <ScrollView>
-            <FlexboxLayout flexDirection="column">
-                <StackLayout row="0" height="500" class="utmArea">
-                        <GridLayout columns="auto, *, auto, auto, auto, auto" rows="50" class="table table-standings">
-                            <Label text="날짜" textWrap="true" row="0" col="0" class="th no"></Label>
-                            <Label text="부서" textWrap="true" row="0" col="1" class="th id"></Label>
-                            <Label text="이름" textWrap="true" row="0" col="2" class="th name"></Label>
-                            <Label text="타입" textWrap="true" row="0" col="3" class="th sn"></Label>
-                        </GridLayout>
-                        <ListView for="item in listOfItems" >
-                            <v-template>
-                                <GridLayout columns="auto, *, auto, auto, auto, auto" rows="40" class="table table-list">
-                                    <Label :text="item.date" textWrap="true" row="0" col="0" class="th no"></Label>
-                                    <Label :text="item.dept_name" textWrap="true" row="0" col="1" class="th id"></Label>
-                                    <Label :text="item.name" textWrap="true" row="0" col="2" class="th name"></Label>
-                                    <Label :text="item.out_office_name" textWrap="true" row="0" col="3" class="th sn"></Label>
-                                </GridLayout>
-                            </v-template>
-                        </ListView>
+    <Page class="page" actionBarHidden="true">
+        <ScrollView class="mainContents">
+            <StackLayout height="100%" width="100%">
+                <StackLayout orientation="horizontal"  marginBottom="5">
+                    <Label text="iCon"  @tap="onDrawerButtonTap" marginRight="5"></Label>
+                    <Label text="근태현황"></Label>
                 </StackLayout>
 
-            </FlexboxLayout>
+                 <ListView for="item in listOfItems" @itemTap="onItemTap"  separatorColor="transparent" 
+                            class="itemList">
+                    <v-template>
+                        <CommuteListComp :item="item" />
+                    </v-template>
+                </ListView>
+            </StackLayout>
         </ScrollView>
 
     </Page>
@@ -50,15 +23,20 @@
     import * as utils from "~/service/utils/utils";
     import SelectedPageService from "~/service/utils/selected-page-service";
     import * as CommuteService from  '../service/commute/CommuteService';
+    import CommuteListComp from "../components/commute_status/Commute-List-Comp";
 
     export default {
         mounted() {
-            SelectedPageService.getInstance().updateSelectedPage("Utm");
+            SelectedPageService.getInstance().updateSelectedPage("Commute");
             CommuteService.commuteToday().then((response) => {
                 var result = response.data;
                 this.listOfItems = result;
             });
         },
+        components : {
+            CommuteListComp
+        },
+
         data() {
             return {
                 listOfItems: []
@@ -67,12 +45,19 @@
         methods: {
             onDrawerButtonTap() {
                 utils.showDrawer();
+            },
+
+            onItemTap(e){
+                console.log(e.item);
             }
         }
     };
 </script>
 
 <style scoped>
+    .mainContents{
+        margin: 5;
+    }
     .utmArea {
         border: 1;
         border-color: black;
